8ca9699838435b0f67f63113f123d625592dfe7e
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2026-02-24 18:57:59 +0530

    feat: Conditionally set API base URL to production or development based on environment.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { auth } from "@/firebase/firebaseClient";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = import.meta.env.PROD
+  ? "https://api.neverbe.lk"
+  : "http://localhost:8080";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
